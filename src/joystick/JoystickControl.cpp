3bff9f9f2aeee6723b6190aa16f2021c5b33557e
Dustin Crossman <dustin.crossman@gmail.com>
2016-09-13 01:31:06 -0700

    Added joystick driver node and joystick control node.
    The driver node publishes raw joystick data out using a custom
    joystick message.
    
    The control node takes the raw data and converts it to a control
    message for use with our control system.
    
    Also added launch and param (settings) files for each node.

---
+++ src/joystick/JoystickControl.cpp
@@ -0,0 +1,123 @@
+#include "ros/ros.h"
+#include "ros/console.h"
+#include "std_msgs/Float64.h"
+#include "robosub/joystick.h"
+#include "robosub/control.h"
+
+ros::Publisher pub;
+
+// Settings
+double min_depth = 0.0;
+double max_depth = 0.0;
+double axisXdeadzone = 0.0;
+double axisYdeadzone = 0.0;
+double axisZdeadzone = 0.0;
+double x_scaling_power = 0.0;
+double y_scaling_power = 0.0;
+double z_scaling_power = 0.0;
+
+/*
+void dead_scale(double *value, double deadzone, double scaling_power)
+{
+    double num;
+    num = (fabs(*value) - deadzone);
+    if( num < 0)
+        num = 0;
+
+    double sgn = (*value > 0) ? 1.0 : -1.0;
+    *value = pow( num/(1-deadzone), scaling_power ) * sgn;
+}
+*/
+
+void joystickToControlCallback(const robosub::joystick msg)
+{
+
+    //scale and apply deadzones
+    //dead_scale(msg.axisX, axisXdeadzone, x_scaling_power);
+    //dead_scale(msg.axisY, axisYdeadzone, y_scaling_power);
+    //dead_scale(msg.axisZ, axisZdeadzone, z_scaling_power);
+
+    //Generate a control packet out of the type to send to control module
+    robosub::control outmsg;
+    outmsg.forward_state = outmsg.STATE_ERROR;
+    outmsg.strafe_state  = outmsg.STATE_ERROR;
+    outmsg.dive_state    = outmsg.STATE_ABSOLUTE;
+    outmsg.yaw_state     = outmsg.STATE_RELATIVE;
+    outmsg.forward = (double)msg.axisX;
+    outmsg.strafe_right = msg.axisY;
+    outmsg.yaw_right = msg.axisZ;
+    outmsg.dive = (double) -((max_depth-min_depth)*(double)msg.throttle + min_depth);
+
+    if (msg.axisZ)
+    {
+        outmsg.yaw_state = outmsg.STATE_RELATIVE;
+        outmsg.yaw_right = msg.axisZ;
+    }
+    else
+    {
+        outmsg.yaw_state = outmsg.STATE_NONE;
+        outmsg.yaw_right = 0;
+    }
+
+    if (!msg.buttons[0])
+    {
+        if (msg.hatX)
+        {
+            outmsg.pitch_state = outmsg.STATE_RELATIVE;
+            outmsg.pitch_up = (double) msg.hatX * -10;
+        }
+        else
+        {
+            outmsg.pitch_state = outmsg.STATE_NONE;
+            outmsg.pitch_up = 0;
+        }
+        if (msg.hatY)
+        {
+            outmsg.roll_state  = outmsg.STATE_RELATIVE;
+            outmsg.roll_right = (double) msg.hatY * 10;
+        }
+        else
+        {
+            outmsg.roll_state = outmsg.STATE_NONE;
+            outmsg.roll_right = 0;
+        }
+
+    }
+    else
+    {
+        outmsg.pitch_state = outmsg.STATE_ABSOLUTE;
+        outmsg.roll_state  = outmsg.STATE_ABSOLUTE;
+        outmsg.yaw_state  = outmsg.STATE_ABSOLUTE;
+        outmsg.roll_right = 0;
+        outmsg.pitch_up = 0;
+        outmsg.yaw_right = 0;
+    }
+    outmsg.yaw_right *= -15;
+    outmsg.forward *= 2;
+
+	pub.publish(outmsg);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "joystick_control");
+
+    ros::NodeHandle nh;
+
+    // Load settings
+    nh.getParam("min_depth", min_depth);
+    nh.getParam("max_depth", max_depth);
+    nh.getParam("axisXdeadzone", axisXdeadzone);
+    nh.getParam("axisYdeadzone", axisYdeadzone);
+    nh.getParam("axisZdeadzone", axisZdeadzone);
+    nh.getParam("x_scaling_power", x_scaling_power);
+    nh.getParam("y_scaling_power", y_scaling_power);
+    nh.getParam("z_scaling_power", z_scaling_power);
+
+    ros::Subscriber sub = nh.subscribe("joystick_driver", 1, joystickToControlCallback);
+    pub = nh.advertise<robosub::control>("joystick_control", 1);
+
+    ros::spin();
+
+    return 0;
+}
---
+++ src/joystick/JoystickDriver.cpp
@@ -0,0 +1,128 @@
+#include "joystick/JoystickDriver.hpp"
+
+JoystickDriver::JoystickDriver(ros::NodeHandle *nh)
+{
+    node = nh;
+    AXIS_MAX = 32767;
+
+    if(!(node->getParam("device", device)))
+    {
+        device = "/dev/input/js0";
+    }
+    ROS_INFO("device: %s", device.c_str());
+
+    fd = open(device.c_str(), O_RDONLY | O_NONBLOCK);
+
+    if (fd < 0)
+    {
+        ROS_DEBUG("could not open joystick: %s", device.c_str());
+        exit(1);
+        return;
+    }
+}
+
+robosub::joystick JoystickDriver::GetJoystickMessage()
+{
+    while(read(fd, &e, sizeof(e)) > 0)
+    {
+        parse_event();
+    }
+    if (errno != EAGAIN) //check for any errors
+    {
+        //std::cerr << "JOYSTICK READ ERROR: " << string(strerror(errno)) << std::endl;
+        ROS_DEBUG("JOYSTICK READ ERROR: %s\n", std::string(strerror(errno)).c_str());
+        exit(1);
+    }
+
+    // Create joystick msg
+    robosub::joystick js_msg;
+    //ROS_DEBUG("joystick_data.axisX: %f\n", joystick_data.axisX);
+
+    js_msg.axisX = (double)joystick_data.axisX;
+    js_msg.axisY = (double)joystick_data.axisY;
+    js_msg.axisZ = (double)joystick_data.axisZ;
+
+    js_msg.hatX = joystick_data.hatX;
+    js_msg.hatY = joystick_data.hatY;
+
+    js_msg.throttle = (double)joystick_data.throttle;
+
+    for(int i=0; i<12; ++i)
+    {
+        js_msg.buttons[i] = joystick_data.button[i];
+    }
+
+    //normalize all of the values
+    js_msg.axisX = js_msg.axisX / (double)AXIS_MAX;
+    js_msg.axisY = js_msg.axisY / (double)AXIS_MAX;
+    js_msg.axisZ = js_msg.axisZ / (double)AXIS_MAX;
+    js_msg.throttle = (((double)joystick_data.throttle/AXIS_MAX)+1)/2;
+    js_msg.hatX /= AXIS_MAX;
+    js_msg.hatY /= AXIS_MAX;
+
+    js_msg.axisX *= -1.0;
+    js_msg.axisX = (js_msg.axisX == -0.0) ? 0.0 : js_msg.axisX;
+
+    return js_msg;
+}
+
+void JoystickDriver::parse_event()
+{
+    switch(e.type & ~JS_EVENT_INIT)
+    {
+        case JS_EVENT_BUTTON:
+            joystick_data.button[e.number] = e.value;
+            break;
+        case JS_EVENT_AXIS:
+            switch(e.number)
+            {
+                case 0: //left_right_axis, left is negative
+                    joystick_data.axisY = e.value;
+                    break;
+                case 1: //forward_back_axis, forward is negative
+                    joystick_data.axisX = e.value;
+                    break;
+                case 2: //twist axis, left is negative
+                    joystick_data.axisZ = e.value;
+                    break;
+                case 3: //throttle, up is negative
+                    joystick_data.throttle = e.value;
+                    break;
+                case 4: //hat left-right, left is negative
+                    joystick_data.hatY = e.value;
+                    break;
+                case 5: //hat up-down, forward is negative
+                    joystick_data.hatX = e.value;
+                    break;
+            }
+            break;
+    }
+}
+
+void JoystickDriver::shutdown()
+{
+    close(fd);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "joystick_driver");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub;
+    pub = nh.advertise<robosub::joystick>("joystick_driver", 1);
+
+    ros::Rate r(10);
+
+    JoystickDriver joystick(&nh);
+    while(ros::ok())
+    {
+        pub.publish(joystick.GetJoystickMessage());
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    joystick.shutdown();
+
+    return 0;
+}
