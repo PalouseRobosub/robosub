c08d1f7cb4c4b2073729e37e4003cd34fc32cbcd
Unknown <apersonnamedmichelle@yahoo.com>
2016-09-19 18:32:11 -0700

    sabertooth thruster node started
    started sabertooth thruster node

---
+++ src/movement/thruster_sabertooth.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "utility/serial.hpp"
+#include "robosub/thruster.h"
+
+using namespace rs;
+
+void callBack(const robosub::thruster::ConstPtr& msg)
+{
+	robosub::thruster message = *msg;
+	for (int i = 0; i < message.data.size(); ++i)
+		ROS_INFO("%lf", message.data[i]);
+}
+
+int main(int argc, char **argv)
+{
+	Serial s;
+
+	ros::init(argc, argv, "thruster");
+
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("thruster", 1, callBack);
+
+	ros::spin();
+
+	return 0;
+}
