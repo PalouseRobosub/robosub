67e87d166498a75eebc0b6178323c3a1213d0481
Sean Kallaher <seanp@kallaher.org>
2016-09-23 14:00:24 -0700

    Commit for testing on the sub with supply status values

--- scripts/upsNode.py
+++ scripts/upsNode.py
@@ -4,6 +4,29 @@ import PyNUT
 import rospy
 from sensor_msgs.msg import BatteryState
 
+def upsDataToChargeStatus(status):
+    if "CHRG" in status:
+        return POWER_SUPPLY_STATUS_CHARGING
+    if "DISCHRG" in status:
+        return POWER_SUPPLY_STATUS_DISCHARGING
+    if not status:
+        return POWER_SUPPLY_STATUS_UNKNOWN
+    
+    return POWER_SUPPLY_STATUS_NOT_CHARGING
+#    return {
+#        'CHRG':1,
+#        'OB DISCHRG':2,
+#        'OB':3,
+#        'OL':4
+#    }.get(x,0)
+
+#def upsDataToHealth(status):
+#    if "HB" in status:
+#        return POWER_SUPPLY_HEALTH_OVERVOLTAGE
+#    if "LB" in status:
+#        return POWER_SUPPLY_HEALTH_
+#    if ""
+
 def upsNode():
     pub = rospy.Publisher('power/ups', BatteryState, queue_size=1)
 
@@ -23,6 +46,9 @@ def upsNode():
             state.present = True
             state.voltage = float(vars['battery.voltage'])
             state.charge = float(vars['battery.charge'])
+            state.power_supply_technology = POWER_SUPPLY_TECHNOLOGY_LIFE
+            
+            state.power_supply_status = upsDataToChargeStatus(vars['ups.status']);
 
 
         rospy.loginfo("Sending msg");
