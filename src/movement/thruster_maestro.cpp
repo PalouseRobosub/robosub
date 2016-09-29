7e17827b9985c877eec0b9392c49c3591635390c
Unknown <nathan.young.wsu@gmail.com>
2016-09-29 16:47:31 -0700

    Tinkered with layout with maestro thruster controller. Exact math needs to be tuned.

--- src/movement/thruster_maestro.cpp
+++ src/movement/thruster_maestro.cpp
@@ -3,20 +3,43 @@
 #include "robosub/thruster.h"
 
 using namespace rs;
+Serial outmessage;
+
+uint16_t translatemessage (const double data)
+{
+	return 4*((data+1)*(500) + 1000);
+}
 
 void Callback (const robosub::thruster::ConstPtr& msg)
 {
-    robosub::thruster message = *msg;
-    for (int i = 0; i < message.data.size(); i++)
-        ROS_INFO("%lf", message.data[i]);
+robosub::thruster message = *msg;
+uint8_t serial_data[40];
+for (int i = 0; i < message.data.size(); i++)
+	{
+		serial_data[0] = 132;
+		serial_data[1] = i;
+		uint16_t rawvalue = translatemessage(message.data[i]);
+		serial_data[2] = rawvalue & 0xff;
+		serial_data[3] = (rawvalue & 0xff00) >> 8;
+	ROS_INFO("%lf", message.data[i]);
+	outmessage.Write(serial_data, 4);
+	}
 }
+
+
 
 int main(int argc, char **argv)
 {
-    Serial s;
+		std::string thruster_port;
     ros::init(argc, argv, "thruster");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("thruster", 1, Callback);
-    ros::spin();
+		if(!n.getParam("thruster_serial_port", thruster_port))
+    {
+      ROS_FATAL("no serial port specified, exiting!");
+      exit(1);
+    }
+		outmessage.Open(thruster_port.c_str(), B9600);
+		ros::spin();
     return 0;
 }
