7be839458eb1a7a01c1853839295f1959ec97f7f
Brandon Kallaher <brandonj@kallaher.org>
2016-10-08 23:29:09 -0700

    Using ruamel.yaml instead of yaml

--- tools/yaml_format.py
+++ tools/yaml_format.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 import sys
-import yaml
 import pprint
+import ruamel.yaml
 
 filename = sys.argv[1]
 
-y = yaml.safe_load(open(filename, 'r'))
+y = ruamel.yaml.round_trip_load(open(filename, 'r'))
 
-print yaml.dump(y, default_style='', indent=4, default_flow_style=False)
+print ruamel.yaml.round_trip_dump(y, default_style='', indent=4, default_flow_style=False)
