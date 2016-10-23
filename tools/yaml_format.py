929aef44295931592817756d81adc9a9bcf76b9d
Brandon Kallaher <brandonj@kallaher.org>
2016-10-23 13:49:46 -0700

    Did some testing
    Findings:
    Comments are kept even if key value is changed
    Files can immediately be closed after loading in using round_trip_load()
    
    TODO:
    Figure out how to loop over all possible key:value pairs in the nested
    dictionary

--- tools/yaml_format.py
+++ tools/yaml_format.py
@@ -6,21 +6,40 @@ import ruamel.yaml
 import argparse
 
 parser = argparse.ArgumentParser(description='Prettify a yaml file')
-parser.add_argument('-i', '--input', metavar='FILE', type=str, required=True , help='Set the input file')
+parser.add_argument('-i', '--input', metavar='FILE', type=str, required=True,
+                    help='Set the input file')
 
 #Setup the output argument (Automatically opens the input string as a file)
-parser.add_argument('-o', '--output', metavar='FILE', type=argparse.FileType('w',0), required=True , help='Set the output file')
+parser.add_argument('-o', '--output', metavar='FILE', type=str, required=True,
+                    help='Set the output file')
 
 args = parser.parse_args()
+
+# the input and output files must be different
+# Exit if they are the same
+if args.input == args.output:
+    print ("[\033[91mFATAL\033[0m] input and output files are the same!\nExiting...")
+    sys.exit()
+
+outputFile = open(args.output, 'r')
+
+#Read the output file
+initial = ruamel.yaml.round_trip_load(outputFile)
+outputFile.close()
 
 #load the yaml file named by args.input
 i = ruamel.yaml.round_trip_load(open(args.input, 'r'))
 
+# Do parameter replacement here
+initial['control']['proportional']['psi'] = i['control']['proportional']['psi']
+
 #Dump the Prettified yaml to file
-o = ruamel.yaml.round_trip_dump(i, default_style='', indent=4, default_flow_style=False)
+o = ruamel.yaml.round_trip_dump(initial, default_style='', indent=4, default_flow_style=False)
+
+outputFile = open(args.output, 'w')
 
 #Write the output to the file
-args.output.write(o)
+outputFile.write(o)
 
 #Close the output file
-args.output.close()
+outputFile.close()
