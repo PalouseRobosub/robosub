6152148016d589e791be86b9db5a1c7a04010666
James Irwin <irwineffect@gmail.com>
2016-11-02 19:06:03 -0700

    added python publisher/subscriber example

---
+++ src/examples/pub_sub.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+
+class Node():
+    def __init__(self):
+        self.sub = rospy.Subscriber('degrees', Float32, self.callback)
+        self.pub = rospy.Publisher('radians', Float32, queue_size=1)
+
+    def callback(self, degrees):
+        radians = degrees.data * 3.14/180
+        self.pub.publish(Float32(radians))
+        
+
+
+
+
+if __name__ == "__main__":
+    rospy.init_node('degree_converter')
+    node = Node()
+    rospy.spin()
