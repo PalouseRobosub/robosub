6565efa99fff36041679496e68fce3d9800c38b3
Brandon Kallaher <brandonj@kallaher.org>
2016-11-05 01:17:13 -0700

    Now Overriding the default behavior for dumping strings. Better checking for keys in the output file. Still need to figure out how to override the YAML sequence formatting (i.e. the thrusters section in cobalt.yaml

--- tools/yaml_format.py
+++ tools/yaml_format.py
@@ -5,6 +5,41 @@ import pprint
 import ruamel.yaml
 import argparse
 
+# This function overrides the default behavior for writing back strings
+#   The modified behavior is to add " around strings
+def process_scalar(self):
+    if self.analysis is None:
+        self.analysis = self.analyze_scalar(self.event.value)
+    if self.style is None:
+        self.style = self.choose_scalar_style()
+    split = (not self.simple_key_context)
+    # VVVVVVVVVVVVVVVVVVVV added
+    if split:  # not a key
+        is_string = True
+        if self.event.value and self.event.value.lstrip('-').replace('.','',1).isdigit():
+            is_string = False
+        # insert extra tests for scalars that should not be ?
+        if is_string:
+            self.style = "\""
+    # ^^^^^^^^^^^^^^^^^^^^
+    # if self.analysis.multiline and split    \
+    #         and (not self.style or self.style in '\'\"'):
+    #     self.write_indent()
+    if self.style == '"':
+        self.write_double_quoted(self.analysis.scalar, split)
+    elif self.style == '\'':
+        self.write_single_quoted(self.analysis.scalar, split)
+    elif self.style == '>':
+        self.write_folded(self.analysis.scalar)
+    elif self.style == '|':
+        self.write_literal(self.analysis.scalar)
+    else:
+        self.write_plain(self.analysis.scalar, split)
+    self.analysis = None
+    self.style = None
+    if self.event.comment:
+        self.write_post_comment(self.event)
+
 #This function is a recursive way to traverse through the nested dictionaries
 #  of yaml files.
 # x is the dictionary to take values from
@@ -12,46 +47,53 @@ import argparse
 def recurse(x,y):
     for k,v in x.items():
         if isinstance(v,dict):
-            recurse(v,y[k])
+            if k in y:
+                recurse(v,y[k])
         else:
-            y[k] = v
+            # If the key is in the destination overwrite else do nothing
+            if k in y:
+                y[k] = v
 
-parser = argparse.ArgumentParser(description='Prettify a yaml file')
-parser.add_argument('-i', '--input', metavar='FILE', type=str, required=True,
-                    help='Set the input file')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Prettify a yaml file')
+    parser.add_argument('-i', '--input', metavar='FILE', type=str, required=True,
+                        help='Set the input file')
 
-#Setup the output argument (Automatically opens the input string as a file)
-parser.add_argument('-o', '--output', metavar='FILE', type=str, required=True,
-                    help='Set the output file')
+    #Setup the output argument (Automatically opens the input string as a file)
+    parser.add_argument('-o', '--output', metavar='FILE', type=str, required=True,
+                        help='Set the output file')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# the input and output files must be different
-# Exit if they are the same
-if args.input == args.output:
-    print ("[\033[91mFATAL\033[0m] input and output files are the same!\nExiting...")
-    sys.exit()
+    # the input and output files must be different
+    # Exit if they are the same
+    if args.input == args.output:
+        print ("[\033[91mFATAL\033[0m] input and output files are the same!\nExiting...")
+        sys.exit()
 
-outputFile = open(args.output, 'r')
+    outputFile = open(args.output, 'r')
 
-#Read the output file
-initial = ruamel.yaml.round_trip_load(outputFile)
-outputFile.close()
+    #Read the output file
+    initial = ruamel.yaml.round_trip_load(outputFile)
+    outputFile.close()
 
-#load the yaml file named by args.input
-i = ruamel.yaml.round_trip_load(open(args.input, 'r'))
+    #load the yaml file named by args.input
+    i = ruamel.yaml.round_trip_load(open(args.input, 'r'))
 
-# Do parameter replacement here
-recurse(i, initial)
+    # Do parameter replacement here
+    recurse(i, initial)
 
+    #Override the Dumper's behavior with strings
+    dd = ruamel.yaml.RoundTripDumper
+    dd.process_scalar = process_scalar
 
-#Dump the Prettified yaml to file
-o = ruamel.yaml.round_trip_dump(initial, default_style='', indent=4, default_flow_style=False)
+    #Dump the Prettified yaml to file
+    o = ruamel.yaml.dump(initial, default_style='', indent=4, Dumper=dd,
+                                    block_seq_indent=1, default_flow_style=False)
+    outputFile = open(args.output, 'w')
 
-outputFile = open(args.output, 'w')
+    #Write the output to the file
+    outputFile.write(o)
 
-#Write the output to the file
-outputFile.write(o)
-
-#Close the output file
-outputFile.close()
+    #Close the output file
+    outputFile.close()
