44f696be8d31225bf9ed0c765ffa71ef976fdf84
ryan <summers.ryan.m@gmail.com>
2016-12-05 20:36:25 -0800

    Corrected bno example to make use of variable speeds and use correct macro

--- src/sensors/bno_example.cpp
+++ src/sensors/bno_example.cpp
@@ -18,9 +18,10 @@ using namespace rs;
  */
 #define HandleError(x, s) \
 { \
-    if ((x)) \
+    int t = (x); \
+    if (t) \
     { \
-        cout << s << " returned " << (x) << endl; \
+        cout << s << " returned " << t << endl; \
         int ret = bno.getSystemStatus(sys_stat, sys_err); \
         if (ret == 0) \
         { \
@@ -56,29 +57,30 @@ int main()
 
     HandleError(bno.setOperationMode(Bno055::OperationMode::Ndof), "Bno055::setOperationMode()");
 
-    /*
-     * Perform a calibration of all sensors.
-     */
-    uint8_t acc_calib = 0, gyr_calib = 0, mag_calib = 0, sys_calib = 0;
-    do
-    {
-        HandleError(bno.getSensorCalibration(Bno055::Sensor::Accelerometer, acc_calib), "Bno055::getSensorCalibration()");
-        cout << "Accelerometer Calibration: " << static_cast<int>(acc_calib) << endl;
-        HandleError(bno.getSensorCalibration(Bno055::Sensor::Gyroscope, gyr_calib), "Bno055::getSensorCalibration()");
-        cout << "Gyroscope Calibration: " << static_cast<int>(gyr_calib) << endl;
-        HandleError(bno.getSensorCalibration(Bno055::Sensor::Magnometer, mag_calib), "Bno055::getSensorCalibration()");
-        cout << "Magnometer Calibration: " << static_cast<int>(mag_calib) << endl;
-        HandleError(bno.getSystemCalibration(sys_calib), "Bno055::getSystemCalibration()");
-        cout << "System Calibration: " << static_cast<int>(sys_calib) << endl;
-        usleep(500000);
-    }
-    while (sys_calib < 3 || acc_calib < 3 || mag_calib < 3 || gyr_calib < 3);
+//    /*
+//     * Perform a calibration of all sensors.
+//     */
+//    uint8_t acc_calib = 0, gyr_calib = 0, mag_calib = 0, sys_calib = 0;
+//    do
+//    {
+//        HandleError(bno.getSensorCalibration(Bno055::Sensor::Accelerometer, acc_calib), "Bno055::getSensorCalibration()");
+//        cout << "Accelerometer Calibration: " << static_cast<int>(acc_calib) << endl;
+//        HandleError(bno.getSensorCalibration(Bno055::Sensor::Gyroscope, gyr_calib), "Bno055::getSensorCalibration()");
+//        cout << "Gyroscope Calibration: " << static_cast<int>(gyr_calib) << endl;
+//        HandleError(bno.getSensorCalibration(Bno055::Sensor::Magnometer, mag_calib), "Bno055::getSensorCalibration()");
+//        cout << "Magnometer Calibration: " << static_cast<int>(mag_calib) << endl;
+//        HandleError(bno.getSystemCalibration(sys_calib), "Bno055::getSystemCalibration()");
+//        cout << "System Calibration: " << static_cast<int>(sys_calib) << endl;
+//        usleep(500000);
+//    }
+//    while (sys_calib < 3 || acc_calib < 3 || mag_calib < 3 || gyr_calib < 3);
 
     double w = 0, x = 0, y = 0, z = 0;
     double roll, pitch, yaw;
+    int rate = 50;
     while (1)
     {
-        sleep(1);
+        usleep(1.0/rate*1000000);
         HandleError(bno.readQuaternion(w, x, y, z), "Bno055::readQuaternion()");
         cout << "w: " << w << endl;
         cout << "x: " << x << endl;
