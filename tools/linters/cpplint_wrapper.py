f257e2fe9ad8473a0bfdc6e3d4c8caf84d3da30b
Brandon Kallaher <brandonj@kallaher.org>
2016-12-09 09:37:24 -0800

    Fixed linter errors in the linter

--- tools/linters/cpplint_wrapper.py
+++ tools/linters/cpplint_wrapper.py
@@ -39,13 +39,15 @@ import re
 
 
 def patch(original_module):
-    """ Decorator to easily allow wrapping/overriding of the Check* functions in cpplint. Should
-        decorate a function which matches the signature of the function it replaces expect with
-        the addition of a fn parameter, which is a pass-through of the replaced function, in case
-        the replacement would like call through to the original functionality. """
+    """ Decorator to easily allow wrapping/overriding of the Check* functions
+        in cpplint. Should decorate a function which matches the signature of
+        the function it replaces except with the addition of a fn parameter,
+        which is a pass-through of the replaced function, in case the
+        replacement would like call through to the original functionality. """
     def wrap(override_fn):
         original_fn = getattr(original_module, override_fn.__name__)
-        setattr(original_module, override_fn.__name__, partial(override_fn, original_fn))
+        setattr(original_module, override_fn.__name__, partial(override_fn,
+                original_fn))
 
         # Don't actually modify the function being decorated.
         return override_fn
@@ -53,8 +55,8 @@ def patch(original_module):
 
 
 def makeErrorFn(original_fn, suppress_categories, suppress_message_matches):
-    """ Create a return a wrapped version of the error-report function which suppresses specific
-        error categories. """
+    """ Create a return a wrapped version of the error-report function which
+        suppresses specific error categories. """
     def newError(filename, linenum, category, confidence, message):
         if category in suppress_categories:
             return
@@ -73,13 +75,14 @@ def GetHeaderGuardCPPVariable(fn, filename):
 
 @patch(cpplint)
 def CheckBraces(fn, filename, clean_lines, linenum, error):
-    """ Complete replacement for cpplint.CheckBraces, since the brace rules for ROS C++ Style
-        are completely different from the Google style guide ones. """
+    """ Complete replacement for cpplint.CheckBraces, since the brace rules
+        for ROS C++ Style are completely different from the Google style guide
+        ones. """
     line = clean_lines.elided[linenum]
     if Match(r'^(.*){(.*)}.?$', line):
         # Special case when both braces are on the same line together, as is the
-        # case for one-line getters and setters, for example, or rows of a multi-
-        # dimenstional array initializer.
+        # case for one-line getters and setters, for example, or rows of a
+        # multi-dimenstional array initializer.
         pass
     else:
         # Line does not contain both an opening and closing brace.
@@ -91,7 +94,8 @@ def CheckBraces(fn, filename, clean_lines, linenum, error):
                 pass
             else:
                 error(filename, linenum, 'whitespace/braces', 4,
-                      'when starting a new scope, { should be on a line by itself')
+                      'when starting a new scope, { should be on a line by \
+                        itself')
         m = Match(r'^(.*)}(.*)$', line)
         if m and (not IsBlankLine(m.group(1)) or not IsBlankLine(m.group(2))):
             if m.group(2) != ";":
@@ -102,16 +106,18 @@ def CheckBraces(fn, filename, clean_lines, linenum, error):
 
 @patch(cpplint)
 def CheckIncludeLine(fn, filename, clean_lines, linenum, include_state, error):
-    """ Run the function to get include state, but suppress all the errors, since
-        ROS C++ Style is silent on include order, and contains no prohibition on use of streams. """
+    """ Run the function to get include state, but suppress all the errors,
+        since ROS C++ Style is silent on include order, and contains no
+        prohibition on use of streams. """
     fn(filename, clean_lines, linenum, include_state,
-       makeErrorFn(error, ['build/include_order', 'build/include_alpha', 'readability/streams'], []))
+       makeErrorFn(error, ['build/include_order', 'build/include_alpha',
+                           'readability/streams'], []))
 
 
 @patch(cpplint)
 def CheckSpacing(fn, filename, clean_lines, linenum, nesting_state, error):
-    """ Do most of the original Spacing checks, but suppress the ones related to braces, since
-        the ROS C++ Style rules are different. """
+    """ Do most of the original Spacing checks, but suppress the ones related
+        to braces, since the ROS C++ Style rules are different. """
     fn(filename, clean_lines, linenum, nesting_state,
        makeErrorFn(error, ['readability/braces', 'whitespace/braces'], []))
 
