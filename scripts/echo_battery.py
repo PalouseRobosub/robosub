635ab7c63b9ef20ed8b4fa17a4cc8c423ca96ff9
Brandon Kallaher <brandonj@kallaher.org>
2016-12-12 22:06:57 -0800

    Made PYLINT happy

--- scripts/echo_battery.py
+++ scripts/echo_battery.py
@@ -4,24 +4,24 @@ import rospy
 from robosub.msg import BatteryDetailed
 from datetime import timedelta
 '''
-' This function returns a string representing the status of the Battery 
+' This function returns a string representing the status of the Battery
 '''
 def statusToString(status):
     string = ""
     for i in status:
         string += {
-                    0:"\x1b[31mUNKNOWN\x1b[0m",
-                    1:"\x1b[36mCHARGING\x1b[0m",
-                    2:"\x1b[34mDISCHARGING\x1b[0m",
-                    3:"\x1b[33mNOT CHARGING\x1b[0m",
-                    4:"\x1b[32mFULL\x1b[0m",
-                    5:"\x1b[31OVERLOAD\x1b[0m",
-                    6:"ON LINE", 
-                    7:"ON BATTERY",
-                    8:"\x1b[31mLOW BATTERY\x1b[0m",
-                    9:"\x1b[31mHIGH BATTERY\x1b[0m",
-                    10:"\x1b[1;31mREPLACE BATTERY\x1b[0m"\
-                  }.get(i,"\x1b[31mUNKNOWN\x1b[0m")
+                    0: "\x1b[31mUNKNOWN\x1b[0m",
+                    1: "\x1b[36mCHARGING\x1b[0m",
+                    2: "\x1b[34mDISCHARGING\x1b[0m",
+                    3: "\x1b[33mNOT CHARGING\x1b[0m",
+                    4: "\x1b[32mFULL\x1b[0m",
+                    5: "\x1b[31OVERLOAD\x1b[0m",
+                    6: "ON LINE",
+                    7: "ON BATTERY",
+                    8: "\x1b[31mLOW BATTERY\x1b[0m",
+                    9: "\x1b[31mHIGH BATTERY\x1b[0m",
+                    10: "\x1b[1;31mREPLACE BATTERY\x1b[0m"
+                  }.get(i, "\x1b[31mUNKNOWN\x1b[0m")
     return string
 
 '''
@@ -31,12 +31,15 @@ def statusToString(status):
 def callback(msg):
     print "UPS Battery:"
     print "Alive: {}".format(msg.alive)
-    print "Inputs:  Current: {: 9f} Voltage: {: 9f}".format(msg.currentInput, msg.voltageInput)
-    print "Outputs: Current: {: 2f} Voltage: {: 2f}".format(msg.currentOutput, msg.voltageOutput)
-    print "Battery: Current: {: 2f} Voltage: {: 2f}".format(msg.currentBattery, msg.voltageBattery)
+    print "Inputs:  Current: {: 9f} Voltage: {: 9f}".format(msg.currentInput,
+                                                            msg.voltageInput)
+    print "Outputs: Current: {: 2f} Voltage: {: 2f}".format(msg.currentOutput,
+                                                            msg.voltageOutput)
+    print "Battery: Current: {: 2f} Voltage: {: 2f}".format(msg.currentBattery,
+                                                            msg.voltageBattery)
     print "Status: {}".format(statusToString(msg.status))
     print u"Temperature: {: 2f} \u00b0F".format(float(msg.temperature))
-    print "Runtime Left: {}".format(timedelta(seconds = msg.runtime.secs))
+    print "Runtime Left: {}".format(timedelta(seconds=msg.runtime.secs))
     print "Percent Available: {} %".format(msg.percentage*100)
     print "Charge: {} Capacity: {}".format(msg.charge, msg.capacity)
 
@@ -46,8 +49,9 @@ def callback(msg):
 if __name__ == "__main__":
 
     # Subscribe to the UPS battery topic
-    sub = rospy.Subscriber('power/ups', BatteryDetailed, callback=callback, queue_size=1)
-    
+    sub = rospy.Subscriber('power/ups', BatteryDetailed,
+                           callback=callback, queue_size=1)
+
     # Initialize the node (Anonymously)
     rospy.init_node('battery_echo', anonymous=True)
 
--- scripts/upsNode.py
+++ scripts/upsNode.py
@@ -5,7 +5,7 @@ import rospy
 from robosub.msg import BatteryDetailed
 
 def upsDataToChargeStatus(status):
-    statuses = status.split() # Split on spaces
+    statuses = status.split()  # Split on spaces
     ret = []
 
     for i in statuses:
@@ -29,20 +29,6 @@ def upsDataToChargeStatus(status):
         return BatteryDetailed.POWER_SUPPLY_STATUS_UNKNOWN
 
     return ret
-#    return {
-#        'CHRG':1,
-#        'OB DISCHRG':2,
-#        'OB':3,
-#        'OL':4
-#    }.get(x,0)
-
-#def upsDataToHealth(status):
-#    if "HB" in status:
-#        return POWER_SUPPLY_HEALTH_OVERVOLTAGE
-#    if "LB" in status:
-#        return POWER_SUPPLY_HEALTH_
-#    if "RB" in status:
-#        return POWER_SUPPLY_HEALTH_
 
 def upsNode():
     pub = rospy.Publisher('power/ups', BatteryDetailed, queue_size=1)
@@ -64,17 +50,19 @@ def upsNode():
         else:
             state.alive = True
             state.voltageBattery = float(vars['battery.voltage'])
-            state.charge = (float(vars['battery.charge']) / 100.0) * float(vars['battery.capacity'])
             state.capacity = float(vars['battery.capacity'])
+            state.charge = ((float(vars['battery.charge']) / 100.0) *
+                            state.capacity)
             state.currentBattery = float(vars['battery.current'])
 
             state.status = upsDataToChargeStatus(vars['ups.status'])
 
             state.percentage = float(vars['battery.charge']) / 100.0
 
-            state.temperature = ((9 * float(vars['battery.temperature'])) / 5) + 32
+            state.temperature = (((9 * float(vars['battery.temperature'])) /
+                                 5) + 32)
 
-            state.runtime = rospy.Time(secs = int(vars['battery.runtime']))
+            state.runtime = rospy.Time(secs=int(vars['battery.runtime']))
 
             state.voltageInput = float(vars['input.voltage'])
             state.currentInput = float(vars['input.current'])
@@ -86,5 +74,5 @@ def upsNode():
         pub.publish(state)
         rate.sleep()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     upsNode()
