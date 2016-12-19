2382259fa410ee272077e00feb82cc53249aa868
Brandon Kallaher <brandonj@kallaher.org>
2016-12-18 16:24:59 -0800

    Initial commit of histogram tool

---
+++ scripts/histogram.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import rospy
+import subprocess
+import importlib
+import signal
+
+def sig_handler(signum, frame):
+    global h
+    h.plot()
+
+class Histogram():
+
+    def __init__(self, arguments):
+        self.args = arguments
+
+        # Determine type of topic we are to listen to
+        output = subprocess.Popen(['rostopic', 'type', self.args.topic], stdout=subprocess.PIPE).communicate()[0]
+        type = output.split('/')
+        i = importlib.import_module(type[0] + ".msg", type[1])
+
+        self.sub = rospy.Subscriber(self.args.topic, getattr(i, type[1][:-1]), callback=self.callback)
+        print "Subscribed to {}".format(self.args.topic)
+        self.data = []
+
+    def callback(self, msg):
+        print "Got Message"
+        self.data.append(eval('msg.' + args.attribute))
+
+    def plot(self):
+        n, bins, patches = plt.hist(self.data, 50, normed=args.normed, facecolor=args.color, alpha=args.alpha)
+
+        if args.normed:
+            plt.ylabel('Probability')
+        else:
+            plt.ylabel('Number')
+
+        plt.xlabel('Value')
+        plt.title('Histogram of {}'.format(args.topic))
+
+        plt.show()
+
+if __name__ == "__main__":
+    global h
+
+    signal.signal(signal.SIGINT, sig_handler)
+
+    rospy.init_node("Histogram", anonymous=True)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('topic', help="Topic to plot data from")
+    parser.add_argument('attribute', help="Attribute of the message to plot")
+    parser.add_argument('--normed', action='store_true', default=False, 
+                        help="Normalize the data in the Histogram")
+    parser.add_argument('--color', default='blue',
+                        help="Choose the color of the bars in the plot")
+    parser.add_argument('--alpha', type=float, default=0.75,
+                        help="Alpha of the color to use")
+    args = parser.parse_args()
+
+    h = Histogram(args)
+
+    rospy.spin()
