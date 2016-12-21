825dacb4d28de2d44e575428d87ef528d832de51
Dustin Crossman <dustin.crossman@gmail.com>
2016-12-21 01:58:08 -0800

    Beginning work on localization system using hydrophones.

--- src/sensors/localization.cpp
+++ src/sensors/localization.cpp
@@ -1,4 +1,5 @@
 #include "localization_system.hpp"
+#include <eigen3/Eigen/Dense>
 
 /*
 msg/PositionsStamped.msg
@@ -7,13 +8,16 @@ float32[] distances
 geometry_msgs/Vector3[] positions
 */
 
+using namespace Eigen;
+
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "localization");
 
     ros::NodeHandle n;
 
-    LocalizationSystem loc_system(1.0/33.0, 100);
+    LocalizationSystem loc_system(1.0/33.0, 5);
 
     // TODO: Change to use localization messages
     ros::Publisher loc_pub = n.advertise<geometry_msgs::Vector3>("pf_position", 1);
@@ -23,6 +27,7 @@ int main(int argc, char **argv)
 
     ros::Subscriber depth_sub = n.subscribe("depth", 1, &LocalizationSystem::depthCallback, &loc_system);
     ros::Subscriber hydrophones_position_sub = n.subscribe("hydrophones/position", 1, &LocalizationSystem::hydrophoneCallback, &loc_system);
+    ros::Subscriber accel_sub = n.subscribe("rs_lin_accel_data", 1, &LocalizationSystem::linAccelCallback, &loc_system);
 
     ros::Rate r(40.0);
 
@@ -30,11 +35,30 @@ int main(int argc, char **argv)
     {
         ros::spinOnce();
         loc_system.Update();
-
         //geometry_msgs::Vector3 pos = loc_system.GetLocalizationMessage();
         //loc_pub.publish(pos);
-
         r.sleep();
+
+
+        //ROS_INFO_STREAM(loc_system.randn() << " : " << loc_system.randu());
+
+        //std::vector<double> t = {1.1, 5.6, 1.1, 0.0, 100.0};
+        //ROS_INFO_STREAM("t: " << t);
+        //ROS_INFO_STREAM("CumSum(t): " << CumSum(t));
+
+        //Matrix<double,6,1> state;
+        //Matrix<double,4,1> obs;
+
+        //state(0,0) = 28.0;
+        //state(1,0) = 10.0;
+        //state(2,0) = -3.0;
+        //state(3,0) = state(4,0) = state(5,0) = 0.0;
+
+        //obs = loc_system.state_to_observation(state);
+        //ROS_INFO_STREAM("obs: " << obs);
+
+        //obs = loc_system.add_observation_noise(obs);
+        //ROS_INFO_STREAM("obs: " << obs);
     }
 
     return 0;
--- src/sensors/localization_system.cpp
+++ src/sensors/localization_system.cpp
@@ -5,10 +5,13 @@ LocalizationSystem::LocalizationSystem(double _dt, int _num_particles)
     dt = _dt;
     num_particles = _num_particles;
 
-    new_hydrophone = new_depth = false;
+    new_hydrophone = new_depth = new_lin_velocity = false;
 
     norm_distribution = new std::normal_distribution<double>(0.0, 1.0);
     uniform_distribution = new std::uniform_real_distribution<double>(0.0, 1.0);
+
+    //std::chrono::high_resolution_clock::time_point t = std::chrono::high_resolution_clock::now();
+    //rand_generator.seed(static_cast<int>(t.time_since_epoch()));
 
     InitializeParticleFilter();
 }
@@ -19,8 +22,24 @@ LocalizationSystem::~LocalizationSystem()
     delete uniform_distribution;
 }
 
+geometry_msgs::Vector3 LocalizationSystem::GetLocalizationMessage()
+{
+    geometry_msgs::Vector3 s;
+    s.x = est_state[0];
+    s.y = est_state[1];
+    s.z = est_state[2];
+
+    return s;
+}
+
 void LocalizationSystem::InitializeParticleFilter()
 {
+    num_iterations = 0;
+
+    lin_velocity.x = 0.0;
+    lin_velocity.y = 0.0;
+    lin_velocity.z = 0.0;
+
     system_update_model.setZero();
     system_update_covar.setZero();
     initial_state.setZero();
@@ -30,17 +49,10 @@ void LocalizationSystem::InitializeParticleFilter()
     est_state.setZero();
     last_est_state.setZero();
 
-    initial_state(0,0) = 10.0;
-    initial_state(1,0) = 10.0;
-    initial_state(2,0) = -2.0;
-    initial_state(3,0) = 0.0;
-    initial_state(4,0) = 0.0;
-    initial_state(5,0) = 0.0;
-
     // TODO: Tune
     // measurement covariance matrix along diagnol
     // variance of each [x_acl, y_acl, z_acl, depth] on diagnal
-    measurement_covar = measurement_covar.Identity() * 0.2;
+    measurement_covar = measurement_covar.Identity() * 3.0;
 
     // system_update_model =
     // | 1  0  0  dt 0  0  |
@@ -60,6 +72,12 @@ void LocalizationSystem::InitializeParticleFilter()
     //system_update_model(1,4) = dt;
     //system_update_model(2,5) = dt;
 
+    // Intial std_devs of position around pinger
+    initial_distribution.setZero();
+    initial_distribution(0,0) = 5.0;
+    initial_distribution(1,0) = 5.0;
+    initial_distribution(2,0) = -2.0;
+
     // TODO: Tune
     // system_update_covariance =
     system_update_covar = system_update_covar.Identity() * 0.2;
@@ -68,12 +86,14 @@ void LocalizationSystem::InitializeParticleFilter()
     {
         // TODO: Randomly distribute initial particle_states
         Matrix<double,6,1> s;
-        for(int j=0; j<est_state.rows(); j++)
+        for(int j=0; j<initial_state.rows(); j++)
         {
-            s(j,0) = initial_state(j,0) + randn() * std::sqrt(system_update_covar(j,j));
+            s(j,0) = initial_state(j,0) + randn() * initial_distribution(j,0);
             particle_states.push_back(s);
         }
         last_particle_states = particle_states;
+
+        ROS_INFO_STREAM("initial_particle_states: " << particle_states);
 
         // Zeroed at this point
         particle_obs.push_back(observation);
@@ -105,12 +125,26 @@ void LocalizationSystem::hydrophoneCallback(const robosub::PositionsStamped::Con
     new_hydrophone = true;
 }
 
+void LocalizationSystem::linAccelCallback(const geometry_msgs::Vector3::ConstPtr &msg)
+{
+    // This is the glorified dead reckoning part...
+    lin_velocity.x += msg->x;
+    lin_velocity.y += msg->y;
+    lin_velocity.z += msg->z;
+
+    //observation(3,0) = lin_velocity.x;
+    //observation(4,0) = lin_velocity.y;
+    //observation(5,0) = lin_velocity.z;
+
+    new_lin_velocity = true;
+}
+
 bool LocalizationSystem::resetFilterCallback(std_srvs::Empty::Request &req, std_srvs::Empty::Response &rep)
 {
     return true;
 }
 
-Matrix<double,4,1> LocalizationSystem::StateToObservation(Matrix<double,6,1> state)
+Matrix<double,4,1> LocalizationSystem::state_to_observation(Matrix<double,6,1> state)
 {
     Matrix<double,4,1> obs;
 
@@ -125,7 +159,7 @@ Matrix<double,4,1> LocalizationSystem::StateToObservation(Matrix<double,6,1> sta
     return obs;
 }
 
-Matrix<double,4,1> LocalizationSystem::AddObservationNoise(Matrix<double,4,1> particle_obs)
+Matrix<double,4,1> LocalizationSystem::add_observation_noise(Matrix<double,4,1> particle_obs)
 {
     return particle_obs + (sqrt_elementwise(measurement_covar) * randn_mat(particle_obs.rows(), 1));
 }
@@ -133,23 +167,25 @@ Matrix<double,4,1> LocalizationSystem::AddObservationNoise(Matrix<double,4,1> pa
 void LocalizationSystem::Update()
 {
     // Getto ros message filter
-    // Should work for now since bno sends its data up all at one time
-    if(new_hydrophone && new_depth)
+    // TODO: This probably doesn't work anymore. Possibly run iteration on any
+    // new sensor reading, keeping old readings same.
+    if(new_hydrophone && new_depth && new_lin_velocity)
     {
-        ROS_INFO_STREAM_THROTTLE(2.0, "=========================================");
+        PRINT_THROTTLE(ROS_INFO_STREAM("==================================");)
+        PRINT_THROTTLE(ROS_INFO_STREAM("num_iterations: " << num_iterations);)
         double particle_weights_sum = 0.0;
         for(int i=0; i<num_particles; i++)
         {
             // Update each particle_states state based on the update model plus noise
             particle_states[i] = system_update_model * last_particle_states[i] + system_update_covar * randn_mat(6,1);
-            ROS_INFO_STREAM_THROTTLE(2.0, "particle_states[" << i << "]: " << particle_states[i]);
+            //PRINT_THROTTLE(ROS_INFO_STREAM("particle_states[" << i << "]: " << particle_states[i]);)
 
             // Update each particle_states observation based on its state
-            particle_obs[i] = StateToObservation(particle_states[i]);
-            ROS_INFO_STREAM_THROTTLE(2.0, "particle_obs[" << i << "]: " << particle_obs[i]);
+            particle_obs[i] = state_to_observation(particle_states[i]);
+            //PRINT_THROTTLE(ROS_INFO_STREAM("particle_obs[" << i << "]: " << particle_obs[i]);)
             // add noise to observations
-            particle_obs[i] = AddObservationNoise(particle_obs[i]);
-            ROS_INFO_STREAM_THROTTLE(2.0, "particle_obs + obs_noise[" << i << "]: " << particle_obs[i]);
+            particle_obs[i] = add_observation_noise(particle_obs[i]);
+            //PRINT_THROTTLE(ROS_INFO_STREAM("particle_obs + obs_noise[" << i << "]: " << particle_obs[i]);)
 
             // This is dense but it is the multivariate normal distribution function found here:
             // https://en.wikipedia.org/wiki/Multivariate_normal_distribution
@@ -160,10 +196,10 @@ void LocalizationSystem::Update()
                     measurement_covar.inverse() * diff ) / std::sqrt(
                     (2.0*3.14*measurement_covar).determinant() );
 
-            //ROS_INFO_STREAM_THROTTLE(1.0, "particle_obs[i]: " << particle_obs[i]);
-            //ROS_INFO_STREAM_THROTTLE(1.0, "observation: " << observation);
-            //ROS_INFO_STREAM_THROTTLE(1.0, "diff: " << diff);
-            ROS_INFO_STREAM_THROTTLE(2.0, "particle_weights[" << i << "]: " << particle_weights[i]);
+            //PRINT_THROTTLE(ROS_INFO_STREAM("particle_obs[i]: " << particle_obs[i]);)
+            //PRINT_THROTTLE(ROS_INFO_STREAM("observation: " << observation);)
+            //PRINT_THROTTLE(ROS_INFO_STREAM("diff: " << diff);)
+            //PRINT_THROTTLE(ROS_INFO_STREAM("particle_weights[" << i << "]: " << particle_weights[i]);)
 
             // Need to get sum for normalization
             particle_weights_sum += particle_weights[i];
@@ -183,15 +219,14 @@ void LocalizationSystem::Update()
             }
         }
 
-        //ROS_INFO_STREAM_THROTTLE(1.0, "particle_weights: " << particle_weights);
         last_particle_states = particle_states;
         last_particle_weights = particle_weights;
         // Resample particles
         //
         // TODO: Explain algorithm
-        //ROS_INFO_STREAM_THROTTLE(1.0, "particle_weights: " << particle_weights);
+        PRINT_THROTTLE(ROS_INFO_STREAM("particle_weights: " << particle_weights);)
         std::vector<double> weights_cumsum = CumSum(particle_weights);
-        //ROS_INFO_STREAM_THROTTLE(1.0, "weights_cumsum: " <<  weights_cumsum);
+        PRINT_THROTTLE(ROS_INFO_STREAM("weights_cumsum: " <<  weights_cumsum);)
         int i, j;
         double threshold;
         for(i=0; i<num_particles; i++)
@@ -215,19 +250,11 @@ void LocalizationSystem::Update()
         {
             est_state += particle_states[i] * particle_weights[i];
         }
+        est_state[3] = est_state[4] = est_state[5] = 0.0;
 
-        // Print everything
-        for(int i=0; i<num_particles; i++)
-        {
-            //ROS_INFO_STREAM_THROTTLE(1.0, "particle [" << i << "]: ");
-            //ROS_INFO_STREAM_THROTTLE(1.0, "STATE" << std::endl << particle_states[i]);
-            //ROS_INFO_STREAM_THROTTLE(1.0, "WEIGHT" << std::endl << particle_weights[i]);
-            //ROS_INFO_STREAM_THROTTLE(1.0, "OBSERVATION: " << std::endl << particle_obs[i] << std::endl);
-        }
-        ROS_INFO_STREAM_THROTTLE(2.0, "particle_weights" << std::endl << particle_weights);
+        PRINT_THROTTLE(ROS_INFO_STREAM("est_state: " << std::endl << est_state);)
 
-        ROS_INFO_STREAM("est_state: " << std::endl << est_state);
-
-        new_hydrophone = new_depth = false;
+        new_hydrophone = new_depth = new_lin_velocity = false;
+        num_iterations++;
     }
 }
