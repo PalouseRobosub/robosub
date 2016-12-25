12a22b631f4b9d5b4a79e9ce882df2dace971d7f
Brandon Kallaher <brandonj@kallaher.org>
2016-12-24 21:56:22 -0800

    modified so that multiple attributes of a single topic could be plotted

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -14,48 +14,63 @@ class Histogram():
 
         self.labels = []
 
-        for i in range(0,len(self.args.topics)):
+        for i in range(0, len(self.args.topics)):
 
-            # Get the class type of the requested topic, blocking until it exists
-            t_type = rostopic.get_topic_class(self.args.topics[i], blocking=True)
+            # Get the class type of the requested topic, block until it exists
+            t_type = rostopic.get_topic_class(self.args.topics[i],
+                                              blocking=True)
 
-            # Subscribe to the topic
-            self.sub = rospy.Subscriber(self.args.topics[i], t_type[0],
-                                        callback=self.callback, callback_args=self.args.topics[i])
+            if self.args.topics[i] not in self.data:
+                # Subscribe to the topic
+                self.sub = rospy.Subscriber(self.args.topics[i], t_type[0],
+                                            callback=self.callback,
+                                            callback_args=self.args.topics[i])
 
-            rospy.loginfo("Subscribed to {}".format(self.args.topics[i]))
+                rospy.loginfo("Subscribed to {}".format(self.args.topics[i]))
 
-            self.data[self.args.topics[i]] = {}
+                self.data[self.args.topics[i]] = {}
 
-            self.data[self.args.topics[i]]['data'] = []
-            self.data[self.args.topics[i]]['attr'] = self.args.attributes[i]
+                self.data[self.args.topics[i]]['data'] = [[]]
+                self.data[self.args.topics[i]]['attr'] = []
+                self.data[self.args.topics[i]]['attr'].append(
+                        self.args.attributes[i])
 
-            self.labels.append(self.args.topics[i] + '/' + self.args.attributes[i])
+            else:
+                self.data[self.args.topics[i]]['data'].append([])
+                self.data[self.args.topics[i]]['attr'].append(
+                        self.args.attributes[i])
+
+            self.labels.append(self.args.topics[i] + '/' +
+                               self.args.attributes[i])
 
         plt.ion()
         plt.show()
 
     def callback(self, msg, topic):
         rospy.logdebug("Got {} Message".format(topic))
-        self.data[topic]['data'].append(eval('msg.' + self.data[topic]['attr']))
+        for i in range(0, len(self.data[topic]['attr'])):
+            self.data[topic]['data'][i].append(eval('msg.' +
+                                                    self.data[topic]['attr'][i])
+                                               )
 
     def plot(self):
         rospy.logdebug("Plotting Update")
         plt_data = []
         for value in self.data.values():
-            plt_data.append(value['data'])
+            for i in range(0, len(value['data'])):
+                plt_data.append(value['data'][i])
 
         plt_data = np.array(plt_data).T
+        print plt_data
 
         try:
             n, bins, patches = plt.hist(plt_data, int(self.args.bins),
-                                        color=['blue', 'red'], histtype='bar',
-                                        normed=1,
+                                        histtype='bar', normed=self.args.normed,
                                         alpha=self.args.alpha,
                                         label=self.labels)
         except ValueError:
-            n, bins, patches = plt.hist(plt_data, self.args.bins, normed=1,
-                                        color=['blue', 'red'], histtype='bar',
+            n, bins, patches = plt.hist(plt_data, self.args.bins,
+                                        normed=self.args.normed, histtype='bar',
                                         alpha=self.args.alpha,
                                         label=self.labels
                                         )
@@ -88,11 +103,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--topics', nargs='+', help="Topic to plot data from")
-    parser.add_argument('--attributes', nargs='+', help="Attribute of the message to plot")
+    parser.add_argument('--attributes', nargs='+',
+                        help="Attribute of the message to plot")
     parser.add_argument('--normed', action='store_true', default=False,
                         help="Normalize the data in the Histogram")
-    parser.add_argument('--color', default='blue',
-                        help="Choose the color of the bars in the plot")
     parser.add_argument('--alpha', type=float, default=0.75,
                         help="Alpha of the color to use")
     parser.add_argument('--bins', default='auto',
