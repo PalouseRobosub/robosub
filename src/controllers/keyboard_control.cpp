a883d45aeee18b1d40c89197246c4f2b05296394
Brandon Kallaher <brandonj@kallaher.org>
2016-12-27 17:55:04 -0800

    Arrow keys fixed and working but better method is available
    Will update with looping over key codes.

--- src/controllers/keyboard_control.cpp
+++ src/controllers/keyboard_control.cpp
@@ -1,6 +1,21 @@
 #include "ros/ros.h"
 #include "robosub/control.h"
 #include <termios.h>
+#include <vector>
+#include <ostream>
+
+using std::vector;
+
+// Used for printing the key codes to the screen for debugging
+std::ostream& operator<<(std::ostream& os, vector<uint8_t> v)
+{
+    for (auto iter = v.begin(); iter < v.end(); ++iter)
+    {
+        os << (int)*iter << ' ';
+    }
+
+    return os;
+}
 
 uint8_t getKey(void)
 {
@@ -41,7 +56,8 @@ int main(int argc, char **argv)
     while(ros::ok())
     {
         robosub::control msg = base_msg;
-        uint8_t key = getKey(), k1, k2;
+        uint8_t key = getKey();
+        vector<uint8_t> keys;
         tcflush(fileno(stdin), TCIFLUSH);
         switch (key)
         {
@@ -70,12 +86,12 @@ int main(int argc, char **argv)
                 msg.dive = 0.25;
                 break;
             case 27:  // Escape Code for special keys
-                k1 = getKey(); // Get second code
-                k2 = getKey(); // Get third code
+                keys.push_back(getKey()); // Get second code
+                keys.push_back(getKey()); // Get third code
 
-                if ('[' == k1)  // Ctrl is not pressed
+                if ('[' == keys[0])  // Ctrl is not pressed
                 {
-                    switch(k2)
+                    switch(keys[1])
                     {
                         case 'A':  // Up Key pressed
                             msg.forward = 1;
@@ -95,11 +111,32 @@ int main(int argc, char **argv)
                         case '6':  // Page down Key pressed
                             msg.dive = -0.25;
                             break;
+                        case '1':
+                            keys.push_back(getKey());
+                            keys.push_back(getKey());
+                            keys.push_back(getKey());
+                            keys.push_back(getKey());
+                            std::cout << keys << std::endl;
+                            switch(keys[4])
+                            {
+                                case 'A':  // Up Key
+                                    msg.pitch_down = 10;
+                                    break;
+                                case 'B':  // Down Key
+                                    msg.pitch_down = -10;
+                                    break;
+                                case 'C':  // Right Key
+                                    msg.yaw_left = -10;
+                                    break;
+                                case 'D':  // Left Key
+                                    msg.yaw_left = 10;
+                                    break;
+                            }
                     }
                 }
-                else if ('O' == k1)  // Ctrl is pressed
+                else if ('O' == keys[0])  // Ctrl is pressed
                 {
-                    switch(k2)
+                    switch(keys[1])
                     {
                         case 'A':  // Up Key pressed
                             msg.pitch_down = 10;
