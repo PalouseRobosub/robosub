522dd2e90ce4fdb6491a160745b0a8a170f30333
rsummers <ryan.summers@wsu.edu>
2017-01-21 18:30:32 -0800

    Addition of stamped depth messages

--- firmware/sensor/src/depth.ino
+++ firmware/sensor/src/depth.ino
@@ -12,36 +12,34 @@
 
 #include <MS5837.h>
 #include <ros.h>
+#include <ros/time.h>
 #include <std_msgs/Float32.h>
+#include "depth_stamped.h"
 #include <Wire.h>
 
 MS5837 ds;
 
 ros::NodeHandle n;
 
-std_msgs::Float32 depth_msg;
+robosub::depth_stamped depth_msg;
 
 ros::Publisher depth_data_pub("depth", &depth_msg);
 
 void setup() {
     n.initNode();
     n.advertise(depth_data_pub);
-    n.loginfo("Starting setup.");
     delay(500);
     ds.init();
     ds.setFluidDensity(997.0f); // fluid density of freshwater
     delay(1000);
-    n.loginfo("Setup Complete.");
 }
 
 void loop()
 {
-    n.loginfo("Reading Depth Sensor.");
     ds.read();
-    depth_msg.data = ds.depth();
+    depth_msg.header.stamp = n.now();
+    depth_msg.depth = ds.depth();
     depth_data_pub.publish(&depth_msg);
-
-    n.loginfo("Spinning.");
 
     n.spinOnce();
 
---
+++ firmware/sensor/src/depth_stamped.h
@@ -0,0 +1,66 @@
+#ifndef _ROS_robosub_depth_stamped_h
+#define _ROS_robosub_depth_stamped_h
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ros/msg.h"
+#include "std_msgs/Header.h"
+
+namespace robosub
+{
+
+  class depth_stamped : public ros::Msg
+  {
+    public:
+      std_msgs::Header header;
+      float depth;
+
+    depth_stamped():
+      header(),
+      depth(0)
+    {
+    }
+
+    virtual int serialize(unsigned char *outbuffer) const
+    {
+      int offset = 0;
+      offset += this->header.serialize(outbuffer + offset);
+      union {
+        float real;
+        uint32_t base;
+      } u_depth;
+      u_depth.real = this->depth;
+      *(outbuffer + offset + 0) = (u_depth.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_depth.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_depth.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_depth.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->depth);
+      return offset;
+    }
+
+    virtual int deserialize(unsigned char *inbuffer)
+    {
+      int offset = 0;
+      offset += this->header.deserialize(inbuffer + offset);
+      union {
+        float real;
+        uint32_t base;
+      } u_depth;
+      u_depth.base = 0;
+      u_depth.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_depth.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_depth.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_depth.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->depth = u_depth.real;
+      offset += sizeof(this->depth);
+     return offset;
+    }
+
+    const char * getType(){ return "robosub/depth_stamped"; };
+    const char * getMD5(){ return "ecaa3eb2663713277f30af306d8347d1"; };
+
+  };
+
+}
+#endif
