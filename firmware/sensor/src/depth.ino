2b7993754cbb3ab8395071ee3e1f7dea5bf988dd
rsummers <ryan.summers@wsu.edu>
2017-01-21 18:54:08 -0800

    Updated arduino to be more ros-interative

--- firmware/sensor/src/depth.ino
+++ firmware/sensor/src/depth.ino
@@ -1,47 +1,74 @@
 /*
  * Author: Zachary Pratt
  *
- * Arduino using Adafruit unified sensor library to get data from the
- * BNO055 9-axis imu sensor and publish the data through ros as a quaternion message.
- *
- * Originally created to test effects of outside magnetic fields on
- * the orientation data out BNO055.
+ * @brief Arduino uno is used for reading the BlueRobotics MS5837 depth sensor.
  */
+#include "Float32Stamped.h"
 
-#include <stdint.h>
 
 #include <MS5837.h>
 #include <ros.h>
-#include <ros/time.h>
 #include <std_msgs/Float32.h>
-#include "depth_stamped.h"
+#include <stdint.h>
 #include <Wire.h>
 
-MS5837 ds;
+MS5837 depth_sensor;
 
 ros::NodeHandle n;
 
-robosub::depth_stamped depth_msg;
+robosub::Float32Stamped depth_msg;
 
 ros::Publisher depth_data_pub("depth", &depth_msg);
 
-void setup() {
+double cycle_delay = 0;
+
+void setup()
+{
     n.initNode();
     n.advertise(depth_data_pub);
+
+    /*
+     * Initialize the depth sensor with the fluid density of
+     * water (997 kg/m^3).
+     */
+    depth_sensor.init();
+    depth_sensor.setFluidDensity(997.0f);
+
+    /*
+     * Delay to allow sensor to power up and ROS node to initialize.
+     */
     delay(500);
-    ds.init();
-    ds.setFluidDensity(997.0f); // fluid density of freshwater
-    delay(1000);
+    while(n.connected() == false)
+    {
+        n.spinOnce();
+    }
+
+    /*
+     * Once the node is initialized, grab the depth rate if it is
+     * available. Otherwise, default to 10Hz and calculate the
+     * delay in milliseconds between loops.
+     */
+    int rate;
+    if (n.getParam("depth/rate", &rate, 1) == false)
+    {
+        rate = 10;
+    }
+
+    if (rate > 20)
+    {
+        rate = 20;
+    }
+
+    cycle_delay = 1 / static_cast<float>(rate) * 1000;
 }
 
 void loop()
 {
-    ds.read();
+    depth_sensor.read();
     depth_msg.header.stamp = n.now();
-    depth_msg.depth = ds.depth();
+    depth_msg.data = depth_sensor.depth();
     depth_data_pub.publish(&depth_msg);
 
     n.spinOnce();
-
-    delay(100);
+    delay(cycle_delay);
 }
