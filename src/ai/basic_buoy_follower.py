d638ebcd4ca0dc58f33f3ee33634570598355f00
James Irwin <irwineffect@gmail.com>
2017-02-04 13:46:02 -0800

    added basic buoy ai task example

--- src/ai/basic_buoy_follower.py
+++ src/ai/basic_buoy_follower.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 import rospy
-from robosub.msg import visionPos as vision_pos
+from robosub.msg import visionPosArray
 from robosub.msg import control
 
 
 class Node():
     def __init__(self):
         self.pub = rospy.Publisher('control', control, queue_size=1)
-        self.sub = rospy.Subscriber('vision/buoy', vision_pos, self.callback)
+        self.sub = rospy.Subscriber('vision_topic', visionPosArray, self.callback)
         self.state = "SEARCHING"
 
     def callback(self, vision_result):
         msg = control()
-        rospy.info("state: {}".format(self.state))
+        rospy.loginfo("state: {}".format(self.state))
         # check for empty message, we can't see a buoy
-        if len(vision_result) = 0:
+        if len(vision_result.data) == 0:
             self.state = "SEARCHING"
             # spin 10 degrees
             msg.yaw_state = control.STATE_RELATIVE
@@ -28,13 +28,13 @@ class Node():
         else:  # we see a buoy, go towards it!
             self.state = "TRACKING"
             msg.yaw_state = control.STATE_ERROR
-            msg.yaw_left = 10 * vision_result[0].xPos
+            msg.yaw_left = -50 * vision_result.data[0].xPos
             msg.dive_state = control.STATE_ERROR
-            msg.dive = 10 * vision_result[0].yPos
+            msg.dive = -1 * vision_result.data[0].yPos
 
             # regulate distance
             msg.forward_state = control.STATE_ERROR
-            error = vision_result[0].magnitude - 0.5
+            error = -50 * (vision_result.data[0].magnitude - 0.012)
             msg.forward = error
 
 
