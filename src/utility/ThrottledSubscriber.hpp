78d8c0b7f98e4bdf163ac0efe3ce87bac8b0e7b8
Brandon Kallaher <brandonj@kallaher.org>
2017-02-17 17:44:15 -0800

    Removed ros_infos and linted

--- src/utility/ThrottledSubscriber.hpp
+++ src/utility/ThrottledSubscriber.hpp
@@ -13,25 +13,26 @@ template <class msg> class ThrottledSubscriber
         ros::Duration rate;
         ros::Subscriber sub;
         void (*callback)(const boost::shared_ptr<const msg>&);
-        
+
         void internalCallback(const boost::shared_ptr<const msg>& message)
         {
             if (nextSubTime <= ros::Time::now())
             {
-                ROS_INFO("Got into internal callback if clause");
                 this->callback(message);
                 nextSubTime = ros::Time::now() + rate;
-                ROS_INFO_STREAM("rate: " << rate << " nextTime: " << nextSubTime);
             }
         }
 
     public:
-        ThrottledSubscriber(const std::string topicName, uint32_t queueSize, void(*callback)(const boost::shared_ptr<const msg>&),
-                            float hz)
+        ThrottledSubscriber(const std::string topicName, uint32_t queueSize,
+                         void(*callback)(const boost::shared_ptr<const msg>&),
+                         float hz)
         {
             ros::NodeHandle nh;
             this->callback = callback;
-            sub = nh.subscribe(topicName, queueSize, &rs::ThrottledSubscriber<msg>::internalCallback, this);
+            sub = nh.subscribe(topicName, queueSize,
+                               &rs::ThrottledSubscriber<msg>::internalCallback,
+                               this);
             this->rate = ros::Duration(1.0/hz);
             nextSubTime = ros::Time::now();
         }
