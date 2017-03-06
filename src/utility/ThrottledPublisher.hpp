b343f82fc3011978d4cb6af51a1159131f91e483
Brandon Kallaher <brandonj@kallaher.org>
2017-03-06 11:51:50 -0800

    Sucessfully exporting libraries and headers

--- src/utility/ThrottledPublisher.hpp
+++ src/utility/ThrottledPublisher.hpp
@@ -12,13 +12,22 @@ template <class msg> class ThrottledPublisher
         ros::Time nextPubTime;
         ros::Duration rate;
         ros::Publisher pub;
+
+        std::string paramName;
+        double paramHz;
+        double lastParamHz;
+
     public:
-        ThrottledPublisher(std::string  topicName, int queueSize, float hz)
+        ThrottledPublisher() {}
+
+        ThrottledPublisher(std::string topicName, int queueSize, float hz, std::string _paramName = "")
         {
             ros::NodeHandle nh;
             pub = nh.advertise<msg>(topicName, queueSize);
-            this->rate = ros::Duration(1.0/hz);
-            nextPubTime = ros::Time::now();
+            setRate(hz);
+
+            paramName = _paramName;
+            getRateParam(paramName);
         }
 
         ~ThrottledPublisher()
@@ -26,11 +35,45 @@ template <class msg> class ThrottledPublisher
 
         void publish(msg message)
         {
+            getRateParam(paramName);
+
             if (nextPubTime <= ros::Time::now())
             {
                 pub.publish(message);
                 nextPubTime = ros::Time::now() + rate;
             }
+        }
+
+        void getRateParam(std::string paramName)
+        {
+            if(paramName != "")
+            {
+                if (!ros::param::getCached(paramName, paramHz))
+                {
+                    ROS_ERROR_STREAM("Failed to load param %s." << paramName);
+                    return;
+                }
+
+                if (paramHz != lastParamHz)
+                {
+                    ROS_INFO_STREAM(paramName << " parameter changed!");
+                    setRate(paramHz);
+                    lastParamHz = paramHz;
+                }
+            }
+        }
+
+        void setRate(float hz)
+        {
+            if(hz != 0.0)
+            {
+                this->rate = ros::Duration(1.0/hz);
+            }
+            else
+            {
+                this->rate = ros::Duration(0.0);
+            }
+            nextPubTime = ros::Time::now();
         }
 };
 };
