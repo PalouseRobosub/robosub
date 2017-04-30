e8513d262326b0311717274bfa8be7dfa81bafd9
Ryan Summers <summers.ryan.m@gmail.com>
2017-04-30 15:56:41 -0700

    Corrected the TRAX after testing

--- src/sensors/PniTrax.cpp
+++ src/sensors/PniTrax.cpp
@@ -1,4 +1,5 @@
 #include "sensors/PniTrax.h"
+#include <netinet/in.h>
 
 /**
  * Constructor.
@@ -9,6 +10,21 @@ PniTrax::PniTrax() :
     automatic_calibration(false),
     output_format(Format::None)
 {
+}
+
+/**
+ * Copies a float from network endianness to little-endian.
+ */
+float ntohf(uint8_t *data)
+{
+    float result;
+    char *result_ptr = reinterpret_cast<char *>(&result);
+    result_ptr[0] = data[3];
+    result_ptr[1] = data[2];
+    result_ptr[2] = data[1];
+    result_ptr[3] = data[0];
+
+    return result;
 }
 
 /**
@@ -25,6 +41,12 @@ int PniTrax::init(const string serial_port_name, Mode mode)
     Command response;
 
     if (serial_port.Open(serial_port_name.c_str(), B38400))
+    {
+        return -1;
+    }
+
+    usleep(500000);
+    if (flush())
     {
         return -1;
     }
@@ -161,18 +183,28 @@ int PniTrax::getRPY(float &roll, float &pitch, float &yaw,
         return -1;
     }
 
-    uint8_t data[18] = {0};
+    uint8_t data[200] = {0};
     Command response;
-    if (read_command(response, data, 18) || response != Command::kGetDataResp)
+    if (read_command(response, data, 200) || response != Command::kGetDataResp)
     {
         return -1;
     }
 
-    memcpy(&yaw, &data[0], 4);
-    memcpy(&pitch, &data[4], 4);
-    memcpy(&roll, &data[8], 4);
-    yaw_accuracy = data[12];
-    calibrated = data[13];
+    if (data[0] != 5 || data[1] != static_cast<uint8_t>(Component::kHeading) ||
+        data[6] != static_cast<uint8_t>(Component::kPitch) ||
+        data[11] != static_cast<uint8_t>(Component::kRoll) ||
+        data[16] != static_cast<uint8_t>(Component::kHeadingStatus) ||
+        data[18] != static_cast<uint8_t>(Component::kCalStatus))
+    {
+        output_format = Format::None;
+        return -1;
+    }
+
+    yaw = ntohf(&data[2]);
+    pitch = ntohf(&data[7]);
+    roll = ntohf(&data[12]);
+    yaw_accuracy = data[17];
+    calibrated = data[19];
 
     return 0;
 }
@@ -218,27 +250,41 @@ int PniTrax::getRawReadings(float &accel_x, float &accel_y, float &accel_z,
     /*
      * Read the data response from the sensor.
      */
-    uint8_t data[36];
+    uint8_t data[200];
     Command response;
-    if (read_command(response, data, 36) || response != Command::kGetDataResp)
+    if (read_command(response, data, 200) || response != Command::kGetDataResp)
     {
+        return -1;
+    }
+
+    if (data[0] != 9 || data[1] != static_cast<uint8_t>(Component::kAccelX) ||
+                        data[6] != static_cast<uint8_t>(Component::kAccelY) ||
+                        data[11] != static_cast<uint8_t>(Component::kAccelZ) ||
+                        data[16] != static_cast<uint8_t>(Component::kMagX) ||
+                        data[21] != static_cast<uint8_t>(Component::kMagY) ||
+                        data[26] != static_cast<uint8_t>(Component::kMagZ) ||
+                        data[31] != static_cast<uint8_t>(Component::kGyroX) ||
+                        data[36] != static_cast<uint8_t>(Component::kGyroY) ||
+                        data[41] != static_cast<uint8_t>(Component::kGyroZ))
+    {
+        output_format = Format::None;
         return -1;
     }
 
     /*
      * Move the data from the buffer into the respective memory locations.
      */
-    memcpy(&accel_x, &data[0], 4);
-    memcpy(&accel_y, &data[4], 4);
-    memcpy(&accel_z, &data[8], 4);
+    memcpy(&accel_x, &data[2], 4);
+    memcpy(&accel_y, &data[7], 4);
+    memcpy(&accel_z, &data[12], 4);
 
-    memcpy(&mag_x, &data[12], 4);
-    memcpy(&mag_y, &data[16], 4);
-    memcpy(&mag_z, &data[20], 4);
+    memcpy(&mag_x, &data[17], 4);
+    memcpy(&mag_y, &data[22], 4);
+    memcpy(&mag_z, &data[27], 4);
 
-    memcpy(&gyro_x, &data[24], 4);
-    memcpy(&gyro_y, &data[28], 4);
-    memcpy(&gyro_z, &data[32], 4);
+    memcpy(&gyro_x, &data[32], 4);
+    memcpy(&gyro_y, &data[37], 4);
+    memcpy(&gyro_z, &data[42], 4);
 
     return 0;
 }
@@ -445,7 +491,8 @@ int PniTrax::write_command(const Command cmd, const uint8_t *payload,
      * Write the byte length into the first two bytes of the packet, and then
      * write the command FRAME_ID byte.
      */
-    memcpy(packet, &packet_len, 2);
+    const uint16_t packet_len_ne = htons(packet_len);
+    memcpy(packet, &packet_len_ne, 2);
     packet[2] = static_cast<uint8_t>(cmd);
 
     /*
@@ -456,8 +503,8 @@ int PniTrax::write_command(const Command cmd, const uint8_t *payload,
     /*
      * Calculate the CRC-16 for the packet and append it to the end.
      */
-    const uint16_t crc = crc16(packet, packet_len - 2);
-    memcpy(&packet[packet_len - 3], &crc, 2);
+    const uint16_t crc = htons(crc16(packet, packet_len - 2));
+    memcpy(&packet[packet_len - 2], &crc, 2);
 
     /*
      * Write the data down the serial port.
@@ -495,6 +542,7 @@ int PniTrax::read_command(Command &resp, uint8_t *payload,
 
     uint16_t packet_len;
     memcpy(&packet_len, data, 2);
+    packet_len = ntohs(packet_len);
 
     /*
      * Packets are limited to 4096 bytes in length.
@@ -516,9 +564,10 @@ int PniTrax::read_command(Command &resp, uint8_t *payload,
      * Calculate and check the crc16 on the packet.
      */
     uint16_t crc;
-    memcpy(&data[packet_len - 3], &crc, 2);
+    memcpy(&crc, &data[packet_len - 2], 2);
+    crc = ntohs(crc);
 
-    if (crc != crc16(data, packet_len))
+    if (crc != crc16(data, packet_len - 2))
     {
         return -1;
     }
@@ -541,20 +590,57 @@ int PniTrax::read_command(Command &resp, uint8_t *payload,
 }
 
 /**
- * Checksum calculator.
+ * CRC16 CCITT Calculator.
+ *
+ * @note Code was taken from:
+ * stackoverflow.com/questions/10564491/function-to-calculate-a-crc16-checksum
  *
  * @param data A pointer to the buffer to be summed.
  * @param length The length of data in bytes pointed to by data.
  *
  * @return The final checksum value.
  */
-uint16_t PniTrax::crc16(uint8_t *data, const int length)
+static const unsigned short crc_table[256] = {
+        0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+        0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+        0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+        0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+        0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+        0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+        0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+        0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+        0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+        0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+        0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+        0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+        0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+        0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+        0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+        0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+        0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+        0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+        0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+        0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+        0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+        0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+        0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+        0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+        0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+        0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+        0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+        0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+        0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+        0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+        0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+        0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0
+};
+uint16_t PniTrax::crc16(uint8_t *data, int length)
 {
-    uint16_t checksum = 0;
+    uint16_t crc = 0x0000;
     for (int i = 0; i < length; ++i)
     {
-        checksum += data[i];
+        crc = ((crc << 8) & 0xFF00) ^ (crc_table[((crc >> 8) & 0xFF) ^ data[i]]);
     }
 
-    return checksum;
+    return crc & 0xFFFF;
 }
