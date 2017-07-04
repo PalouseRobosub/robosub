08a9ffb6b8fd34e0f5173fe725d477be8f0d60d1
Brandon Kallaher <brandonj@kallaher.org>
2017-07-03 23:55:20 -0700

    Added clear state function and short description comments

--- src/movement/control_wrapper.py
+++ src/movement/control_wrapper.py
@@ -2,7 +2,7 @@
 # AUTHOR:   Brandon Kallaher
 # FILE:     control_wrapper.py
 # CREATED:  2017-07-03 23:20:06
-# MODIFIED: 2017-07-03 23:49:59
+# MODIFIED: 2017-07-03 23:54:22
 # DESC:     This class is used to wrap around the control messages for
 #           readability and simplicity. This uses a singleton so that all
 #           instances have the same internal state at any given time and
@@ -24,32 +24,49 @@ class control_wrapper():
     def __init__(self):
         self.control_msg = control()
 
+    # Allow a user to get the current state
     def getCurrentState(self):
-        return self.control_msg 
+        return self.control_msg
 
+    # Yaw state-value pair setter
     def setYawLeft(self, state, value):
         self.control_msg.yaw_state = state
         self.control_msg.yaw_left = value
 
+    # Pitch state-value pair setter
     def setPitchDown(self, state, value):
         self.control_msg.pitch_state = state
         self.control_msg.pitch_down = value
 
+    # Roll state-value pair setter
     def setRollRight(self, state, value):
         self.control_msg.roll_state = state
         self.control_msg.roll_right = value
 
+    # Dive state-value pair setter
     def setDive(self, state, value):
         self.control_msg.dive_state = state
         self.control_msg.dive = value
 
+    # Forward state-value pair setter
     def setForward(self, state, value):
         self.control_msg.forward_state = state
         self.control_msg.forward = value
 
+    # Strafe state-value pair setter
     def setStrafeLeft(self, state, value):
         self.control_msg.strafe_state = state
         self.control_msg.strafe_left = value
+
+    # Allow for clearing of the movement state
+    def clearState(self):
+        self.control_msg = control()
+
+
+
+
+
+
 
 # Main for testing
 if __name__ == "__main__":
@@ -65,3 +82,5 @@ if __name__ == "__main__":
     d = control_wrapper()
     print d
     print d.getCurrentState()
+    d.clearState()
+    print c.getCurrentState()
