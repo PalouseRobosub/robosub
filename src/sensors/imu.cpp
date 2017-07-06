3f1677fe4d2ead369dffaeb1f8d20fd296b01a56
James Irwin <irwineffect@gmail.com>
2017-07-06 15:24:01 -0700

    added generic imu node

--- src/sensors/imu.cpp
+++ src/sensors/imu.cpp
@@ -1,16 +1,55 @@
 #include "ros/ros.h"
+#include "geometry_msgs/QuaternionStamped.h"
+#include "robosub/Euler.h"
+#include "tf/transform_datatypes.h"
 
-ros::Publisher pub;
+ros::Publisher quaternion_publisher;
+ros::Publisher euler_publisher;
+
+robosub::Euler quaternion_to_euler(
+                         const geometry_msgs::QuaternionStamped::ConstPtr &msg)
+{
+    double roll, pitch, yaw;
+    tf::Quaternion tf_quaternion;
+    robosub::Euler euler_msg;
+
+    tf::quaternionMsgToTF(msg->quaternion, tf_quaternion);
+
+    tf::Matrix3x3 m(tf_quaternion);
+    m.getRPY(roll, pitch, yaw);
+
+    euler_msg.roll = roll;
+    euler_msg.pitch = pitch;
+    euler_msg.yaw = yaw;
+
+    return euler_msg;
+}
+
+void orientation_callback(const geometry_msgs::QuaternionStamped::ConstPtr &msg)
+{
+    quaternion_publisher.publish(msg);
+    euler_publisher.publish(quaternion_to_euler(msg));
+}
 
 int main(int argc, char **argv)
 {
-
     ros::init(argc, argv, "imu");
 
     ros::NodeHandle n;
 
-    ROS_INFO_STREAM("name: " << ros::this_node::getName());
-    ROS_INFO_STREAM("namespace: " << ros::this_node::getNamespace());
+    std::string active_imu;
+    if(ros::param::get("~active_imu", active_imu) != true)
+    {
+        ROS_FATAL("~active_imu param not set.");
+        return 1;
+    }
+
+    quaternion_publisher =
+            n.advertise<geometry_msgs::QuaternionStamped>("orientation", 1);
+    euler_publisher = n.advertise<robosub::Euler>("pretty/orientation", 1);
+
+    ros::Subscriber sub = n.subscribe(active_imu + "/orientation", 1,
+                                      orientation_callback);
 
     ros::spin();
 
