53d61e3e248ca6e77419f9dd54e22e47453543c6
Sean Kallaher <seanp@kallaher.org>
2017-07-11 12:47:54 -0700

    Removed delayed_node.sh

--- scripts/delayed_node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script is used for delaying the launching of nodes within a launch file
-# the first parameter is the delay time (in seconds), the second is the executable name
-# usage example:
-# <node name="serial_subscriber" pkg="robosub" type="delayed_node.sh" args="1 serial_subscriber"/>
-sleep $1
-echo "got here"
-rosrun robosub $2
-echo "returned here"
