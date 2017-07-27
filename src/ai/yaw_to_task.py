97538cb743ec9ac6a8b1e4df1ab6a65e54297ddc
Sean Kallaher <seanp@kallaher.org>
2017-07-27 15:23:48 -0700

    Yaw to task

---
+++ src/ai/yaw_to_task.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import rospy
+from rs_yolo.msg import DetectionArray as detection_array
+from robosub.msg import control
+from util import *
+
+import sys
+
+class YawToTask():
+
+    def __init__(self, yaw_val):
+        rospy.loginfo("Init done")
+        self.pub = rospy.Publisher('control', control, queue_size=1)
+        self.state = "SEARCHING"
+
+        msg = control()
+        # Maintain roll and pitch of 0
+        msg.roll_state = control.STATE_ABSOLUTE
+        msg.roll_right = 0
+        msg.pitch_state = control.STATE_ABSOLUTE
+        msg.pitch_down = 0
+        # Don't move forward
+        msg.forward_state = control.STATE_ERROR
+        msg.forward = 1
+
+        msg.dive_state = control.STATE_ABSOLUTE
+        msg.dive = -1
+
+        msg.yaw_state = control.STATE_RELATIVE
+        msg.yaw_left = yaw_val
+        
+        self.pub.publish(msg)
+
+if __name__ == "__main__":
+    rospy.init_node('yaw_to_task')
+    node = YawToTask(sys.argv[1])
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        rate.sleep()
