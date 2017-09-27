75a319d0093e877a53c10a6cf9d3c64162c31562
Sindorman <mikebykhovtsev@gmail.com>
2017-09-26 23:57:30 -0700

    Added foundation logic. Moves forward until it sees at least one gate.

---
+++ src/ai/smach_gate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import rospy
+from util import *
+from rs_yolo.msg import DetectionArray
+from robosub.msg import control
+from SubscribeState import SubscribeState
+from control_wrapper import control_wrapper
+import smach
+import smach_ros
+
+class move_to_gate(SubscribeState):
+    def __init__(self, vision_label, poll_rate=10):
+        SubscribeState.__init__(self, "vision", DetectionArray, self.callback, outcomes=['success'])
+        self.vision_label = vision_label
+        self._poll_rate = rospy.Rate(poll_rate)
+
+    def callback(self, msg):
+        c = control_wrapper()
+        c.levelOut()
+        c.forwardError(10)
+
+        detections = filterByLabel(msg.detections, self.vision_label)
+
+        vision_result = getMostProbable(detections, thresh=0.5)
+
+        normalize(vision_result)
+        while vision_result is None:
+            c.publish()
+            self._poll_rate.sleep()
+        return 'success'
+
+class gate_task(smach.StateMachine):
+    def __init__(self):
+        smach.StateMachine.__init__(self, outcomes=['success'])
+
+        with self:
+            smach.StateMachine.add('FORWARD UNTIL FOUN GATE', move_to_gate('start_gate'),
+                                    transitions={'success': 'success'})
+
+if __name__ == '__main__':
+    rospy.init_node('gate_task')
+    sm = gate_task()
+
+    sis = smach_ros.IntrospectionServer('smach_server', sm, '/SM_ROOT')
+    sis.start()
+    outcomes = sm.execute()
+    sis.stop()
