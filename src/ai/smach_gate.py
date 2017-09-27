1c109f943bc2f11d93d1daee10807df83121496b
Sindorman <mikebykhovtsev@gmail.com>
2017-09-27 00:04:06 -0700

    Added a bit of comments to the code

--- src/ai/smach_gate.py
+++ src/ai/smach_gate.py
@@ -8,7 +8,9 @@ from control_wrapper import control_wrapper
 import smach
 import smach_ros
 
+#Based on move_forward from forward_until.py with tweaks for vision
 class move_to_gate(SubscribeState):
+    #not sure what poll_rate is for
     def __init__(self, vision_label, poll_rate=10):
         SubscribeState.__init__(self, "vision", DetectionArray, self.callback, outcomes=['success'])
         self.vision_label = vision_label
@@ -17,6 +19,7 @@ class move_to_gate(SubscribeState):
     def callback(self, msg):
         c = control_wrapper()
         c.levelOut()
+        #10 is an arbirtary number
         c.forwardError(10)
 
         detections = filterByLabel(msg.detections, self.vision_label)
@@ -24,11 +27,13 @@ class move_to_gate(SubscribeState):
         vision_result = getMostProbable(detections, thresh=0.5)
 
         normalize(vision_result)
+        #move while forward until we see a gate post
         while vision_result is None:
             c.publish()
             self._poll_rate.sleep()
         return 'success'
 
+#High state machine of gate task
 class gate_task(smach.StateMachine):
     def __init__(self):
         smach.StateMachine.__init__(self, outcomes=['success'])
