4e7e7c639054e44c9ad5d55d71ed80fdd17a7068
James Irwin <irwineffect@gmail.com>
2017-09-28 17:39:57 -0700

    added depth info to robot state publisher

--- src/examples/tf_broadcaster.py
+++ src/examples/tf_broadcaster.py
@@ -4,22 +4,29 @@ import rospy
 import tf
 import tf2_ros
 from geometry_msgs.msg import QuaternionStamped, TransformStamped
+from robosub.msg import Float32Stamped
 
 class Node():
     def __init__(self, child_frame):
         self.sub = rospy.Subscriber('orientation', QuaternionStamped,
-                                    self.callback)
+                                    self.orientation_callback)
+        self.sub = rospy.Subscriber('depth', Float32Stamped,
+                                    self.depth_callback)
         self.br = tf2_ros.TransformBroadcaster()
         self.child_frame = child_frame
+        self.depth = 0.0
 
-    def callback(self, orientation):
+    def depth_callback(self, msg):
+        self.depth = msg.data
+
+    def orientation_callback(self, orientation):
         t = TransformStamped()
         t.header.stamp = rospy.Time.now()
         t.header.frame_id = "world"
         t.child_frame_id = self.child_frame
         t.transform.translation.x = 0.0
         t.transform.translation.y = 0.0
-        t.transform.translation.z = 0.0
+        t.transform.translation.z = self.depth
         t.transform.rotation = orientation.quaternion
 
         self.br.sendTransform(t)
