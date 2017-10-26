d6d4d2884fd79dcad2b406acf2af2a58fc155957
Sean R. Williams <sean.r.williams@wsu.edu>
2017-10-26 16:33:01 -0700

    sub_tf_broadcaster now listens to /position/point and uses it to update the cobalt frame accordingly

--- src/sensors/sub_tf_broadcaster.py
+++ src/sensors/sub_tf_broadcaster.py
@@ -3,7 +3,7 @@
 import rospy
 import tf
 import tf2_ros
-from geometry_msgs.msg import QuaternionStamped, TransformStamped
+from geometry_msgs.msg import QuaternionStamped, TransformStamped, Quaternion, PointStamped, Point
 from robosub.msg import Float32Stamped
 
 class Node():
@@ -12,22 +12,38 @@ class Node():
                                     self.orientation_callback)
         self.sub = rospy.Subscriber('depth', Float32Stamped,
                                     self.depth_callback)
+        self.sub = rospy.Subscriber("position/point", PointStamped,
+                                    self.location_callback)
         self.br = tf2_ros.TransformBroadcaster()
         self.child_frame = child_frame
         self.depth = 0.0
+        self.point = Point()
+        self.quaternion = Quaternion()
+        self.update_tf_tree()
 
     def depth_callback(self, msg):
         self.depth = msg.data
+        self.update_tf_tree()
 
     def orientation_callback(self, orientation):
+        self.quaternion = orientation.quaternion
+        self.update_tf_tree()
+
+    def location_callback(self, point):
+        self.point = point.point
+        self.update_tf_tree()
+
+
+
+    def update_tf_tree(self):
         t = TransformStamped()
         t.header.stamp = rospy.Time.now()
         t.header.frame_id = "world"
         t.child_frame_id = self.child_frame
-        t.transform.translation.x = 0.0
-        t.transform.translation.y = 0.0
+        t.transform.translation.x = self.point.x
+        t.transform.translation.y = self.point.y
         t.transform.translation.z = self.depth
-        t.transform.rotation = orientation.quaternion
+        t.transform.rotation = self.quaternion
 
         self.br.sendTransform(t)
 
