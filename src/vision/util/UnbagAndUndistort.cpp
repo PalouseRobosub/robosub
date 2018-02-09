dbd258fe9a2f9709768cbea6350ae1e4f14d38c0
Sindorman <mikebykhovtsev@gmail.com>
2018-02-09 14:46:21 -0800

    Cut off old manual undistortion, changed to the idea of publishing images to undistortion, grabbing and saving. Incomplete - due to idea of bagging undistorted data.

--- src/vision/util/UnbagAndUndistort.cpp
+++ src/vision/util/UnbagAndUndistort.cpp
@@ -27,9 +27,29 @@ using namespace cv;
 using std::string;
 using sensor_msgs::Image;
 
-Mat rectifyMap[2][2];
+void image_callback(const sensor_msgs::Image::ConstPtr& msg)
+{
+    //TODO complete and adjust for main.
+    // save as a png output
+    ros::Time t = m.getTime();
 
-int stereoCropRadius = -1;
+    double secs = t.toSec();
+    string topic_name = m.getTopic();
+
+    int start = topic_name.find("/", 1);
+    int end = topic_name.find("/", start+1);
+
+    int len = end - start - 1;
+
+    string prefix = topic_name.substr(start+1, len);
+
+    string message_name = std::to_string(secs);
+
+    string output_file = output_dir + "/" + prefix + "_" + message_name + ".png";
+    ROS_DEBUG_STREAM("writing to: " << output_file);
+    imwrite(output_file, image_ptr->image);
+
+}
 
 
 int main (int argc, char** argv)
@@ -43,60 +63,10 @@ int main (int argc, char** argv)
 
         ros::init(argc, argv, "undistMono");
 
+        ros::NodeHandle n("~");
+        ros::NodeHandle n_public;
+
         ROS_INFO_STREAM("Init done");
-
-        ros::NodeHandle n;
-        image_transport::ImageTransport it(n);
-
-        string stereoCalibFile = "";
-        n.getParam("stereo_calib_file", stereoCalibFile);
-
-        ROS_INFO_STREAM("Fetched params");
-        ROS_INFO_STREAM("Reading stereo calibration file: " << stereoCalibFile);
-
-        FileStorage fs(stereoCalibFile, FileStorage::READ);
-
-        Mat K1, K2, D1, D2, R1, R2, P1, P2;
-        Size imageSize;
-
-        if (!fs.isOpened())
-        {
-            ROS_FATAL_STREAM("Stereo calibration file could not be opened from: " <<
-                    stereoCalibFile);
-            exit(1);
-        }
-        else
-        {
-            ROS_INFO_STREAM("Valid calibration file");
-            string calibTime;
-            fs["calibration_time"] >> calibTime;
-            fs["crop_radius"] >> stereoCropRadius;
-            ROS_INFO_STREAM("Calibration was performed on: " << calibTime);
-            fs["K1"] >> K1;
-            fs["D1"] >> D1;
-            fs["K2"] >> K2;
-            fs["D2"] >> D2;
-            fs["R1"] >> R1;
-            fs["R2"] >> R2;
-            fs["P1"] >> P1;
-            fs["P2"] >> P2;
-            fs["image_size"] >> imageSize;
-        }
-
-        ROS_INFO_STREAM("Stereo calibration file read");
-
-        ROS_INFO_STREAM("Initializing stereo undistortion maps");
-
-        fisheye::initUndistortRectifyMap(K1, D1, R1, P1,
-                imageSize, CV_16SC2, rectifyMap[0][0],
-                rectifyMap[0][1]);
-
-        fisheye::initUndistortRectifyMap(K2, D2, R2, P2,
-                imageSize, CV_16SC2, rectifyMap[1][0],
-                rectifyMap[1][1]);
-
-        ROS_INFO_STREAM("Completed initialization");
-
         // paths
         string bagPath;
         string output_dir;
@@ -119,12 +89,12 @@ int main (int argc, char** argv)
         if(mkdir(output_dir.c_str(), 0755))
         {
             int error = errno;
-            ROS_ERROR_STREAM("Failed to create path: " << output_dir << 
+            ROS_ERROR_STREAM("Failed to create path: " << output_dir <<
                     "\nreason: " << strerror(error));
 
             exit(1);
         }
-        
+
         ROS_INFO_STREAM("Reading: " << bagPath);
         ROS_INFO_STREAM("Saving in: " << output_dir);
 
@@ -139,54 +109,24 @@ int main (int argc, char** argv)
 
         rosbag::View view(bag, rosbag::TopicQuery(topics));
 
+        image_transport::ImageTransport it(n_public);
+
+        undistorted_publisher = it.advertise("camera/left/undistorted",
+                1);
+        undistorted_publisher = it.advertise("camera/right/undistorted",
+                1);
+
+        image_transport::Subscriber sub = it.subscribe(
+                "camera/left/image_raw", 1, image_callback);
+        image_transport::Subscriber sub = it.subscribe(
+                "camera/right/image_raw", 1, image_callback)
+
         for(rosbag::MessageInstance const m : view)
         {
-            Image::ConstPtr s = m.instantiate<Image>();
 
-            if (s != NULL)
+            if (m.instantiate<Image>() != NULL)
             {
-                // do convert stuff
-                cv_bridge::CvImagePtr image_ptr;
-                image_ptr = toCvCopy(s, sensor_msgs::image_encodings::BGR8);
-
-                Mat temp = image_ptr->image.clone();
-                Mat cropped;
-
-                if (stereoCropRadius != -1)
-                {
-                    Mat cropMask = Mat::zeros(temp.rows, temp.cols, CV_8UC1);
-                    circle(cropMask, Point(temp.size().width / 2,
-                                temp.size().height / 2), stereoCropRadius,
-                            Scalar(255, 255, 255), -1, 8, 0);
-
-                    temp.copyTo(cropped, cropMask);
-                }
-                else
-                {
-                    temp.copyTo(cropped);
-                }
-
-                remap(temp, image_ptr->image, rectifyMap[0][0], rectifyMap[0][1],
-                        INTER_LINEAR);
-
-                // save as a png output
-                ros::Time t = m.getTime();
-
-                double secs = t.toSec();
-                string topic_name = m.getTopic();
-
-                int start = topic_name.find("/", 1);
-                int end = topic_name.find("/", start+1);
-
-                int len = end - start - 1;
-
-                string prefix = topic_name.substr(start+1, len);
-
-                string message_name = std::to_string(secs);
-
-                string output_file = output_dir + "/" + prefix + "_" + message_name + ".png";
-                ROS_DEBUG_STREAM("writing to: " << output_file);
-                imwrite(output_file, image_ptr->image);
+                //TODO To complete requires to publish images to both cameras.
             }
         }
 
