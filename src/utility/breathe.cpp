13148262cf894184cf6c016323069ba30b4160ce
Sindorman <mikebykhovtsev@gmail.com>
2018-02-28 13:29:33 -0800

    Testing of breathe LED pattern.

--- src/utility/breathe.cpp
+++ src/utility/breathe.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
 
 using namespace std;
+ros::Publisher pub;
 
 // TODO finish this program.
-int main()
+int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "LED");
+    ros::NodeHandle n;
 
+    std_msgs::Float32 outmsg;
+    outmsg.data = 0.1;
+    pub = n.advertise<std_msgs::Float32>("led", 1);
+    float change = 0.1;
+    while(true)
+    {
+        for(;outmsg.data != 1.0; outmsg.data += change)
+        {
+            pub.publish(outmsg);
+            sleep(1);
+        }
+        change = -change;
+    }
+    return 0;
 }
