185cf30bc06aafa52f2992b03f2a8ff0bc3e13ff
Chrisnonfer <chris.freeberg@gmail.com>
2018-10-21 10:26:11 -0700

    Added DIVE state

--- src/ai/smach_surface.py
+++ src/ai/smach_surface.py
@@ -11,7 +11,7 @@ class surface_ai(smach.StateMachine):
         smach.StateMachine.__init__(self, outcomes=['success'])
 
         with self:
-            smach.StateMachine.add('DIVE', basic_states.goToDepth(0.5), 
+            smach.StateMachine.add('DIVE', basic_states.goToDepth(-0.5), 
                                   transitions={'success': 'MOVE_TO_PINGER', 'fail': 'FAIL', 'timeout': 'FAIL'})
             smach.StateMachine.add('MOVE_TO_PINGER', roulette_states.MoveToPinger(),
                                   transitions={'success': 'SURFACE', 'fail': 'MOVE_TO_PINGER',
